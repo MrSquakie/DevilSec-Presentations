22132137612f4174140d1992f3a1ad0e570b083b
MrSquakie <MrSquakie09@gmail.com>
2019-10-11 18:36:03 -0400

    modified:   DockerInstall.sh 	new file:   hashes.txt

--- DockerInstall.sh
+++ DockerInstall.sh
@@ -49,20 +49,6 @@ else
 	exit
 fi
 
-
-echo -e "\e[92mTesting docker install...\n"
-echo -e "\e[39m"
-#Test
-docker run hello-world
-if [ $? -eq 0 ] 
-then
-	echo -e "\e[92mSuccess \e[39m"
-else
-	echo -e "\e[91 Error... Aborting."
-	exit
-fi
-
-
 echo -e "\e[92mStarting Docker Service...\n"
 echo -e "\e[39m"
 #run Docker
@@ -88,6 +74,20 @@ else
 	exit
 fi
 
+echo -e "\e[92mTesting docker install...\n"
+echo -e "\e[39m"
+#Test
+docker run hello-world
+if [ $? -eq 0 ] 
+then
+	echo -e "\e[92mSuccess \e[39m"
+else
+	echo -e "\e[91 Error... Aborting."
+	exit
+fi
+
+
+
 
 ##Setup for todays lecture
 echo -e "\e[92mInstalling DVWA Docker container...\n \e[39m"
@@ -101,7 +101,9 @@ then
 	echo -e "\e[92m Opening browser \e[39m"
 	firefox http://127.0.0.1/ &
 else
-	echo -e "\e[91m Browser is open \e[39m"
-	echo -e "\e[33m Please navigate to http://127.0.0.1/  \e[39m"
+	echo -e "\e[91mBrowser is open \e[39m"
+	echo -e "\e[33mPlease navigate to http://127.0.0.1/  \e[39m"
 fi
 
+
+echo -e "\e[92mInstall successfull. Credentials are admin:admin but if you reset the database it will be admin:password \e[39m"
